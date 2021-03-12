ab1e5c89576bc6d73c3450a54913e5d921ea1763
Hung <hung.tx194772@sis.hust.edu.vn>
2021-03-12 14:11:27 +0700

    After class submission

--- Lab03/Code/AimsProject/src/Cart.java
+++ Lab03/Code/AimsProject/src/Cart.java
@@ -1,4 +1,23 @@
 
 public class Cart {
 
+	public static final int MAX_NUMBERS_ORDERED = 20;
+	private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+	
+	public int qtyOrdered = 0;
+	public double Cost = 0.0;
+	
+	public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+		
+	}
+	
+	public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+		
+	}
+	
+	public double totalCost() {
+		
+		return Cost;
+	}
+	
 }
