0fb92bbb0bfc124030da5a70180f1ffeb9135275
Hung <hung.tx194772@sis.hust.edu.vn>
2021-05-14 12:58:07 +0700

    Inclass progress

---
+++ lab10/GUIProject/src/hust/soict/globalict/javafx/PainterController.java
@@ -0,0 +1,27 @@
+package hust.soict.globalict.javafx;
+
+import java.awt.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+public class PainterController {
+
+    @FXML
+    private VBox drawingAreaPane;
+
+    @FXML
+    void drawingAreaMouseDragged(MouseEvent event) {
+    	Circle newCircle = new Circle(event.getX(),
+    			event.getY(), 4, Color.BLACK);
+    	drawingAreaPane.getChildren().add(newCircle);
+    }
+
+    @FXML
+    void clearButtonPressed(ActionEvent event) {
+    	drawingAreaPane.getChildren().clear();
+    }
+
+}
